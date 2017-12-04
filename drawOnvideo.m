9288be62a0e170ee7b8815a2c914875c9dc2aad6
李梓玥 <lzzy@github.com>
2017-12-05 00:44:31 +0800

    Comment added

--- drawOnvideo.m
+++ drawOnvideo.m
@@ -1,10 +1,11 @@
 function drawOnvideo()
-% draw_on_video Summary of this function goes here
+% drawOnVideo Summary of this function goes here
 % 
 % Do real-time webcam processing and displaying
 %
 % In this demo I just draw a yellow circle on the canvas
 
+% the image acquisition hardware is reset
 imaqreset
 % The parameters may differ on distinct OS and PS, such as 'winvideo'
 obj = videoinput('macvideo',1,'YCbCr422_1280x720');
@@ -23,7 +24,9 @@ try
         %% Image processing part goes here
         I = getdata(obj,1);
         I = ycbcr2rgb(I);
+        % Remove all logged data records associated with object
         flushdata(obj);
+        % Here just draw a circle, but other operations can be performed
         I = insertShape(I,'circle',[640 300 100],'Linewidth',5);
 
         %% This is what paints on the canvas
@@ -35,6 +38,7 @@ catch err
     % This attempts to take care of things when the figure is closed
     stop(obj);
     delete(obj);
+    % the image acquisition hardware is reset
     imaqreset
     disp('Cleaned up')
     rethrow(err);
