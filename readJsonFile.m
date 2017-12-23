5ab00a0f4798aedbb6da1265be5701be37ea79ab
李梓玥 <lzzy@github.com>
2017-12-23 16:17:13 +0800

    multiple people readJsonFile

--- readJsonFile.m
+++ readJsonFile.m
@@ -1,6 +1,10 @@
 function [vector] = readJsonFile(jsonform)
 addpath('jsonlab');
 json=loadjson(jsonform);
-pirVal=json.people{1, 1}.pose_keypoints;
-vector = reshape(cell2mat(pirVal),3,18);
+[~,peopleCount] = size(json.people)
+vector = zeros(3,18,peopleCount);
+for i = 1:peopleCount
+    pirVal=json.people{1, i}.pose_keypoints;
+    vector(:,:,i) = reshape(cell2mat(pirVal),3,18);
+end
 
