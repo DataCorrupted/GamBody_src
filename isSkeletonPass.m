1fae3e8e1fae1a3fe566a55bf4bda9f682a82d3d
Peter Rong <PeterRong96@gmail.com>
2017-12-23 14:08:34 +0800

    skeleton pass finished.
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- isSkeletonPass.m
+++ isSkeletonPass.m
@@ -1,11 +1,35 @@
-function [ is_pass ] = isPass( body_path, mask_path )
+function [ is_pass ] = isSkeletonPass( body_path, mask_path )
+%isPass Summary of this function goes here
+%
+% Draw the current state as a feedback to user.
+%
+% Input: 
+% 	body_path: String. A path to body json file.
+% 	mask_path: String. A path to mask json file.
+%
+% Output:
+%	is_pass: bollean. Whether the player passed.
+%
 
-body = readjson(body_path)
-mask = readjson(mask_path)
-
+addpath('jsonlab');
+jsonBody=loadjson(body_path);
+jsonMask=loadjson(mask_path);
+pirValBody=jsonBody.people{1, 1}.pose_keypoints;
+pirValMask=jsonMask.people{1, 1}.pose_keypoints;
+jsonvertor1 = cell2mat(pirValBody);
+jsonvertor2 = cell2mat(pirValMask);
+loss = 0;
+count = 0;
+for i = 1:3:54
+    if jsonvertor1(i+2) >= 0.4 && jsonvertor2(i+2) >= 0.4
+        loss = loss + norm(jsonvertor1(i:i+1)-jsonvertor2(i:i+1));
+        count = count + 1;
+    end
+end
+if loss > 100 || count < 10
+    is_pass = 0;
+else
+    is_pass = 1;
 end
 
-function [ skeleton ] = readjson( filepath )
-	
 end
-
