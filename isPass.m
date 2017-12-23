9b60c367705cc47c735080cc6a4ccc73e5a59b7c
李梓玥 <lzzy@github.com>
2017-12-23 13:46:16 +0800

    fixed: exceptional cases
    no body detected or low confidence

--- isPass.m
+++ isPass.m
@@ -32,12 +32,14 @@ pirValMask=jsonMask.people{1, 1}.pose_keypoints;
 jsonvertor1 = cell2mat(pirValBody);
 jsonvertor2 = cell2mat(pirValMask);
 error = 0;
+count = 0;
 for i = 1:3:48
-    if jsonvertor1(i+2) ~= 0 && jsonvertor2(i+2) ~= 0
+    if jsonvertor1(i+2) >= 0.4 && jsonvertor2(i+2) >= 0.4
         error = error + norm(jsonvertor1(i:i+1)-jsonvertor2(i:i+1));
+        count = count + 1;
     end
 end
-if error > 100
+if error > 100 || count < 10
     is_pass = 0;
 else
     is_pass = 1;
