8d0146fde9f363b1bb44468ed2310506a538b158
Peter Rong <PeterRong96@gmail.com>
2017-12-24 15:25:41 +0800

    `:wq
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- getSkeleton.m
+++ getSkeleton.m
@@ -10,6 +10,6 @@ function [ body_path ] = getSkeleton( img )
 % Output:
 %	body_path: string.
 %
-
+body_path = 'D:\OneDrive\CurrentWork\ComputerVision\Project\GamBody_src\masks\WIN_20171206_14_43_29_Pro_keypoints.json';
 %% Pull Openpose here.
 end
