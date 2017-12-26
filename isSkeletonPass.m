6af31e5dad0b89d87dbf1e94d447e010440aa0d2
Peter Rong <PeterRong96@gmail.com>
2017-12-26 15:21:40 +0800

    finished tuning
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- isSkeletonPass.m
+++ isSkeletonPass.m
@@ -16,17 +16,21 @@ loss = 0;
 count = 0;
 % Suppose size(mask) is same
 [~,points,peopleCount] = size(body);
+% weight = [.15, .1, ...
+%           .05, .03, .07, .05, .03, .07, ...
+%           .1, .07, 0.03, .1, .07, .03, ...
+%           .015, .015, .01, .01];
+weight = ones(1, 18) / 18;
 for i = 1:peopleCount
     mask = mask(:,:,i);
     for j = 1:points
         if body(3,j) >= 0.4 && mask(3,j) >= 0.4
-            loss = loss + norm(body(1:2,j)-mask(1:2,j));
+            loss = loss + norm(body(1:2,j)-mask(1:2,j)) * weight(i);
             count = count + 1;
         end
     end
 end
-
-if loss > 500 || count < 10
+if loss > 20 || count < 10
     is_pass = 0;
 else
     is_pass = 1;
