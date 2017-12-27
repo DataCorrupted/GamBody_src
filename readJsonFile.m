4aeff989b47787f6c1f6f9287f88f57700da2a26
Jianxiong Cai <ernestme@icloud.com>
2017-12-27 11:40:08 +0800

    fix the bug for nobody situation

--- readJsonFile.m
+++ readJsonFile.m
@@ -1,11 +1,13 @@
 function [vector] = readJsonFile(jsonform)
 addpath('jsonlab');
 json=loadjson(jsonform);
-[~,peopleCount] = size(json.people);
+[a,peopleCount] = size(json.people);
 vector = zeros(3,18,peopleCount);
-for i = 1:peopleCount
-    pirVal=json.people{1, i}.pose_keypoints;
-    vector(:,:,i) = reshape(cell2mat(pirVal),3,18);
+if a
+    for i = 1:peopleCount
+        pirVal=json.people{1, i}.pose_keypoints;
+        vector(:,:,i) = reshape(cell2mat(pirVal),3,18);
+    end
 end
 end
 
